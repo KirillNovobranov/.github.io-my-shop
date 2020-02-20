87ee10be279f3b37bf6bc0effc8f33f1d15d8d8f
Kirill Novobranov <megavip01@gmail.com>
2020-02-20 19:20:29 +0200

    added div component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ import React from 'react'
 // const App = () => <h1>Hello world 1</h1>
 
 const App = () => {
-	return <h1>Hello world 1</h1>
+	return <div>
+		<h1>Hello world 1</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellat impedit natus, modi explicabo ullam ratione repudiandae placeat ipsum aspernatur reprehenderit aut accusantium ad eligendi excepturi facere quas deserunt quae!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellat impedit natus, modi explicabo ullam ratione repudiandae placeat ipsum aspernatur reprehenderit aut accusantium ad eligendi excepturi facere quas deserunt quae!
+		</p>
+	</div>
+	
+	
 }
 
 export default App
