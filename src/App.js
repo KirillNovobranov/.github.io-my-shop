27df4277316ecfd32b4c09efbb165da733e94d9c
Kirill Novobranov <megavip01@gmail.com>
2020-02-20 20:02:39 +0200

    created Header, Main, Footer components

--- src/App.js
+++ src/App.js
@@ -15,57 +15,75 @@ import './reset.css'
 const App = () => {
 	return (
 		<div>
-			<header className="header">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-3">
-							<div className="logo">
-								<img src="img/logo.png" alt="" />>
-					</div>
-							</div>
-							<div className="col-lg-6">
-								<div className="menu">
-									<ul>
-										<li><a href="/">Home</a></li>
-										<li><a href="/">Products</a></li>
-										<li><a href="/">Payment</a></li>
-										<li><a href="/">Shipping</a></li>
-									</ul>
-								</div>
-							</div>
-							<div className="col-lg-3">
-								<div className="cart text-center">
-									<div className="products-count">0</div>
-									<div className="products-price">$500</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</header>
-				<main className="main">
-					<div className="container">
-						<div className="row">
-							<div className="col-lg-3">
-
-							</div>
-							<div className="col-lg-9">
-
-							</div>
-						</div>
-					</div>
-				</main>
-				<footer className="footer">
-
-				</footer>
+			<Header />
+			<Main />
+			<Footer />
 		</div>
 		
 	)
 	
 }
 
+const Footer = () => {
+	return (
+		<div>
+			<footer className="footer">
+
+			</footer>
+		</div>
+	)
+}
+
+const Main = () => {
+	return (
+		<div>
+			<main className="main">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+
+						</div>
+						<div className="col-lg-9">
+
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	)
+}
+
 const Header = () => {
 	return (
-		<h1>Hello World</h1>
+		<div>
+			<header className="header">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							<div className="logo">
+								<img src="img/logo.png" alt="" />
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="menu">
+								<ul>
+									<li><a href="/">Home</a></li>
+									<li><a href="/">Products</a></li>
+									<li><a href="/">Payment</a></li>
+									<li><a href="/">Shipping</a></li>
+								</ul>
+							</div>
+						</div>
+						<div className="col-lg-3">
+							<div className="cart text-center">
+								<div className="products-count">0</div>
+								<div className="products-price">$500</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
 	)
 }
 
