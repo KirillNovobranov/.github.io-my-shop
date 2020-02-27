1d8346a0a9d138ca62a6849f023f8a6d0145da74
Kirill Novobranov <megavip01@gmail.com>
2020-02-27 20:42:01 +0200

    added quantity buttons

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -19,6 +19,11 @@ const ProductListItem = ({
             <div className="product-description">{description}</div>
             <div className="product-features">Type: {type}</div>
             <div className="product-features">Capacity: {capacity} Gb</div>
+            <div className="product-quantity">
+                <button>-</button>
+                <input type="text" value="1"/>
+                <button>+</button>
+            </div>
             <div className="product-price">{price} $</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
