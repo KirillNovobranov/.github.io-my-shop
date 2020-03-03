78cbb0d5533dbdefac229c0a116c084486a9ecf0
Kirill Novobranov <megavip01@gmail.com>
2020-03-03 19:34:45 +0200

    destructuring ProductListItem

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -4,21 +4,31 @@ import './ProductListItem.css'
 
 class ProductListItem extends Component {
     render() {
+
+        const {
+            name, 
+            description, 
+            type, 
+            capacity, 
+            price,
+            image
+        } = this.props;
+
         return (
             <div className="product-list-item">
                 <div className="product-img">
-                    <img src={this.props.image} alt={this.props.description} title={this.props.name} height="102px" />
+                    <img src={image} alt={description} title={name} height="102px" />
                 </div>
-                <div className="product-title">{this.props.name}</div>
-                <div className="product-description">{this.props.description}</div>
-                <div className="product-features">Type: {this.props.type}</div>
-                <div className="product-features">Capacity: {this.props.capacity} Gb</div>
+                <div className="product-title">{name}</div>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-quantity">
                     <button>-</button>
                     <input type="text" value="1" readOnly />
                     <button>+</button>
                 </div>
-                <div className="product-price">{this.props.price} $</div>
+                <div className="product-price">{price} $</div>
                 <button className="btn-add-to-cart">Add to cart</button>
             </div>
         )
