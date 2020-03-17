693a1e3db017a2a9505c9df9b0801cdce10d91df
Kirill <megavip01@gmail.com>
2020-03-17 20:29:40 +0200

    started to use reduce in total

--- src/App/Header/Cart/Cart.js
+++ src/App/Header/Cart/Cart.js
@@ -8,23 +8,28 @@ const productsObj = productsData.reduce((accObj, product) => ({
     [product.id]: product,
 }),{})
 
-console.log(productsObj)
-console.log(productsData)
-
 const Cart = ({
    productsInCart
 }) => {
 
     return (
+        
+
         <div className="cart text-center">
             {
                 keys(productsInCart).map((productId) =>(
                     // <div key={productId}>{productsData[productId-1].name} : {productsInCart[productId]}</div>
                     <div key={productId}>{productsObj[productId].name} : {productsInCart[productId]}</div>
+                    
                 ))
-
-            }
-        </div>
+            } <div>
+                Total:
+                {
+                    keys(productsInCart).reduce()
+                } 
+                $
+                </div>
+        </div>        
     )
 }
 
