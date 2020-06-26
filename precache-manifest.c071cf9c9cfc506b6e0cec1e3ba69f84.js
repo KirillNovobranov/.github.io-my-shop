048c02d20ad75f2e63e67b34ac9e421cf0a2c83c
Kirill <megavip01@gmail.com>
2020-06-26 09:06:38 +0300

    Updates

---
+++ precache-manifest.c071cf9c9cfc506b6e0cec1e3ba69f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85e4618d827e88d0e9fedf3f4b000c9",
+    "url": "/my-shop/index.html"
+  },
+  {
+    "revision": "10428a1039e5003dc336",
+    "url": "/my-shop/static/css/main.7925b2e4.chunk.css"
+  },
+  {
+    "revision": "56693c2416cf81c988a5",
+    "url": "/my-shop/static/js/2.3a695dfa.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/my-shop/static/js/2.3a695dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10428a1039e5003dc336",
+    "url": "/my-shop/static/js/main.94784f1e.chunk.js"
+  },
+  {
+    "revision": "9f35796e7e57cdd715ed",
+    "url": "/my-shop/static/js/runtime-main.e7dfa890.js"
+  }
+]);
